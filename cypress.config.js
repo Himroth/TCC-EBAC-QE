391b8ab0baf14a7decbf2c06dee6cac26c0bf5d2
Helton Imroth <heltonimroth@gmail.com>
2023-01-16 15:45:34 -0300

    new cypress config and new cypress API folder

--- cypress.config.js
+++ cypress.config.js
@@ -5,7 +5,7 @@ module.exports = defineConfig({
     baseUrl: "http://lojaebac.ebaconline.art.br/",
     viewportHeight: 1000,
     viewportWidth: 1500,
-    specPattern: 'cypress/UI/**/*.cy.{js,jsx,ts,tsx}',
+    specPattern: 'cypress/**/*.cy.{js,jsx,ts,tsx}',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
