17145aeb85e7b9f6f8441be115683a53b8bee0cb
Helton Imroth <heltonimroth@gmail.com>
2023-01-16 10:49:16 -0300

    new folder and config folde

--- cypress.config.js
+++ cypress.config.js
@@ -5,6 +5,7 @@ module.exports = defineConfig({
     baseUrl: "http://lojaebac.ebaconline.art.br/",
     viewportHeight: 1000,
     viewportWidth: 1500,
+    specPattern: 'cypress/UI/**/*.cy.{js,jsx,ts,tsx}',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
