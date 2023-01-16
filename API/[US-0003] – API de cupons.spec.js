cb64595fb2a99f8f3b8dc8a91567bd990dbbdac2
Helton Imroth <heltonimroth@gmail.com>
2023-01-16 17:17:01 -0300

    new

--- API/[US-0003] – API de cupons.spec.js
+++ API/[US-0003] – API de cupons.spec.js
@@ -1,14 +1,16 @@
 const request = require('supertest');
+const authorization = require('../API/Utils/apiAcess.json')
 
 describe('API de cupons', () => {
+
     it('Realizando um GET de cupons', () => {
         request('http://lojaebac.ebaconline.art.br/wp-json/wc/v3')
             .get('/coupons')
             .set('Accept', 'application/json')
-            .set("Authorization", 'Basic YWRtaW5fZWJhYzpAYWRtaW4hJmJAYyEyMDIy')
+            .set("Authorization", JSON.stringify(authorization))
             .then(response => {
                 expect(response.status).toEqual(200)
-            })
+})
     });
 
     it('Realizando POST de cupom já existente', () => {
@@ -21,9 +23,10 @@ describe('API de cupons', () => {
                 "description": "Cupom de desconto de teste",
             })
             .set('Accept', 'application/json')
-            .set("Authorization", 'Basic YWRtaW5fZWJhYzpAYWRtaW4hJmJAYyEyMDIy')
+            .set("Authorization", JSON.stringify(authorization))
             .then(response=> {
-                expect(response.body.data.status).toEqual(400)
+                //expect(response.body.data.status).toEqual(400)
+                expect(response.status).toEqual(400)
                 expect(response.body.message).toEqual('O código de cupom já existe')
             })
     });
