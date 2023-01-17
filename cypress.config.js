8d0c1f7072548d3efa12e4eeeb72c227a230ba2c
Helton Imroth <heltonimroth@gmail.com>
2023-01-16 21:00:01 -0300

    news repoters

--- cypress.config.js
+++ cypress.config.js
@@ -1,13 +1,23 @@
 const { defineConfig } = require("cypress");
+const { beforeRunHook, afterRunHook } = require('cypress-mochawesome-reporter/lib');
 
 module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
   e2e: {
     baseUrl: "http://lojaebac.ebaconline.art.br/",
     viewportHeight: 1000,
     viewportWidth: 1500,
     specPattern: 'cypress/**/*.cy.{js,jsx,ts,tsx}',
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      on('before:run', async (details) => {
+        console.log('override before:run');
+        await beforeRunHook(details);
+      });
+
+      on('after:run', async () => {
+        console.log('override after:run');
+        await afterRunHook();
+      });
     },
   },
 });
